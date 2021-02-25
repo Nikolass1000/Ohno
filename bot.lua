8641d3ce4a773bd690f2126c2a097b8debc1c1b8
Neu1rO <75801123+Neu1rO@users.noreply.github.com>
2021-02-25 03:39:46 +0300

    Add files via upload

---
+++ bot.lua
@@ -0,0 +1,15 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+local ohno = message.guild.emojis:find(function(e) return e.name == 'ohno' end)
+	if message.content == "ohno" then
+		message:addReaction(ohno)
+	end
+end)
+
+client:run("Bot "..io.open("./token.txt"):read())
